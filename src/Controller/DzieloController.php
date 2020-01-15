0ac823d7b972d0990bc22886374921c645448028
YaggiDev <54006287+YaggiDev@users.noreply.github.com>
2020-01-15 16:03:50 +0100

    Update DzieloController.php

--- src/Controller/DzieloController.php
+++ src/Controller/DzieloController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-use App\Entity\Dzielo;
+use App\Entity\dzielo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
