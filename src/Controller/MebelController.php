3e26594d3458b0781b793a2059fc1a80c453737f
Maciej Sarnacki <44607205+msarnacki@users.noreply.github.com>
2020-01-15 15:44:56 +0100

    Update MebelController.php

--- src/Controller/MebelController.php
+++ src/Controller/MebelController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Controller;
+use App\Entity\Mebel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 class DzieloController extends AbstractController
