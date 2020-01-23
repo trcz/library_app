f9834e6daaacbb458414ebcbf4191af6bdcb9a81
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-01-23 19:25:06 +0100

    Autor

--- src/Controller/DzieloController.php
+++ src/Controller/DzieloController.php
@@ -25,7 +25,7 @@ class DzieloController extends AbstractController
         $data = "Dziady";
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
-            'SELECT p a.nazwisko FROM App\Entity\Dzielo p LEFT JOIN autor_dzielo a
+            'SELECT p FROM App\Entity\Dzielo p LEFT JOIN p.autor_dzielo a
     WHERE p.tytul LIKE :data')
             ->setParameter('data','%'.$data.'%');
 
