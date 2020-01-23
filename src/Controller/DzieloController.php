a3442f8f68c98e3a965d1c143fec250a4d805b8d
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-01-23 19:37:13 +0100

    Autor

--- src/Controller/DzieloController.php
+++ src/Controller/DzieloController.php
@@ -25,7 +25,7 @@ class DzieloController extends AbstractController
         $data = "Dziady";
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
-            'SELECT a FROM App\Entity\Autor_Dzielo p LEFT JOIN p.autor_id a
+            'SELECT p.id a FROM App\Entity\Autor_Dzielo p LEFT JOIN p.autor_id a
     ')
             ->setParameter('data','%'.$data.'%');
 
