98c66b53f6485c1ea8d8c75985c3a4ad647a68c5
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-01-23 16:15:20 +0100

    testing

--- src/Controller/DzieloController.php
+++ src/Controller/DzieloController.php
@@ -110,8 +110,8 @@ class DzieloController extends AbstractController
 
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
-            'SELECT p FROM App\Entity\Dzielo p
-    WHERE p.tytul LIKE :data')
+            'SELECT * FROM App\Entity\Dzielo 
+    WHERE Dzielo.tytul LIKE :data')
             ->setParameter('data',$data);
 
 
