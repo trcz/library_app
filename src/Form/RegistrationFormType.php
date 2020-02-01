b6a81cdefcfd20440061947ed134aa55c60e5f93
Kamil <36967931+trcz@users.noreply.github.com>
2020-02-01 22:34:57 +0100

    Update RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,6 +18,9 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('username', ['label' => false,])
+            ->add('username', [
+                'label' => false,
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'label' => false,
                 'mapped' => false,
