9b6ab971c1de671b5855d1e74d7fe620a69feb81
Kamil <36967931+trcz@users.noreply.github.com>
2020-02-01 22:35:21 +0100

    Update RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,7 +17,6 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', ['label' => false,])
             ->add('username', [
                 'label' => false,
             ])
