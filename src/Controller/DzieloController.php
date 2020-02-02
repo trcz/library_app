a3143119b65f35a320b1062b77b339b13b64d51b
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-02-02 19:08:43 +0100

    menu głowne funkcja

--- src/Controller/DzieloController.php
+++ src/Controller/DzieloController.php
@@ -10,10 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 /**
  * @Route("/dzielo")
+ * @Security("has_role('ROLE_USER')")
  */
 class DzieloController extends AbstractController
 {
