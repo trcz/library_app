e4bea83ed8946197b18cfae2618b7f7e79eb229c
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-02-02 21:39:19 +0100

    security fix

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-
 class AdminController extends AbstractController
 {
     /**
