5657b259e187943b67d98b0b43b21a2aec0b77ff
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-02-02 23:41:52 +0100

    History method and giveBack

--- src/Controller/WypozyczenieController.php
+++ src/Controller/WypozyczenieController.php
@@ -69,11 +69,11 @@ class WypozyczenieController extends AbstractController
     {
         $data = $request->request->get('oddaj');
         $userId = $request->request->get('user_id');
-        $wypozyczenia = $this->getDoctrine()->getRepository(Wypozyczenie::class)->findBy(array('uzytkownik_id'=>$userId));
         $em = $this->getDoctrine()->getManager();
         $wypozyczenie = $em->getRepository(Wypozyczenie::class)->find($data);
         $wypozyczenie->setStatus(0);
         $em->flush();
+        $wypozyczenia = $this->getDoctrine()->getRepository(Wypozyczenie::class)->findBy(array('uzytkownik_id'=>$userId));
         return $this->render('wypozyczenie/historia.html.twig',[
             'wypozyczenies' => $wypozyczenia
         ]);
