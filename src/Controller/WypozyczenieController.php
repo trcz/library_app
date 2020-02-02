e6df2e62a72760960f5215acffe3a36371096d42
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-02-02 22:27:34 +0100

    History method and giveBack

--- src/Controller/WypozyczenieController.php
+++ src/Controller/WypozyczenieController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Bridge\Doctrine\Security\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @Route("/wypozyczenie")
