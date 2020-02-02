923c73d7fba4b0f4134f3c3fc189a6de93369c8d
yaggidev <lukasz.wojciech.jagiello@gmail.com>
2020-02-02 18:57:20 +0100

    menu głowne funkcja

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class RegistrationController extends AbstractController
 {
